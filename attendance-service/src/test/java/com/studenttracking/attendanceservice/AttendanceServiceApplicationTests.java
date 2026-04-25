592c13dd3047ca72f667a2ea815a3a7ac4fbdc65
ajynky <ajynky.joshi@gmail.com>
2026-04-25 20:07:23 +0530

    WDAC-1234 || "Day 3 - All Spring Boot services configured"

---
+++ attendance-service/src/test/java/com/studenttracking/attendanceservice/AttendanceServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.studenttracking.attendanceservice;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Disabled
+class AttendanceServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
